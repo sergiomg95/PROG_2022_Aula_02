75b8a3a511df51afc22155864b3d95604342ba46
Mañana <Maana@Aula8-04>
2024-02-01 11:55:22 +0100

    Setters and getters y vectores:
    Creamos los setters y getters y los vectores(el vector del aula ya
    estaba en el anterior commit)

--- src/Aula/Alumno.java
+++ src/Aula/Alumno.java
@@ -7,4 +7,32 @@ public class Alumno {
 	private int telefono;
 	private Asignatura asignaturas[];
 	
+	
+	
+	public Alumno() {
+		asignaturas=new Asignatura[4];
+	}
+	
+	
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellidos() {
+		return apellidos;
+	}
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	public int getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+	
 }
--- src/Aula/Asignatura.java
+++ src/Aula/Asignatura.java
@@ -6,4 +6,30 @@ public class Asignatura {
 	private int EV1;
 	private int EV2;
 	private int EV3;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getEV1() {
+		return EV1;
+	}
+	public void setEV1(int eV1) {
+		EV1 = eV1;
+	}
+	public int getEV2() {
+		return EV2;
+	}
+	public void setEV2(int eV2) {
+		EV2 = eV2;
+	}
+	public int getEV3() {
+		return EV3;
+	}
+	public void setEV3(int eV3) {
+		EV3 = eV3;
+	}
+	
+	
 }
